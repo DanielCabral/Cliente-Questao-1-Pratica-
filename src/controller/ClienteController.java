7a633662a28681089381ca8676ecf11bfe7a3b0d
DanielCabral <Daniel-cabral14@hotmail.com>
2020-07-12 20:27:07 -0300

    Comunicando com servidor proxy

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,29 +1,18 @@
 package controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Scanner;
-
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.TransferMode;
 
 public class ClienteController implements Initializable {
 	@FXML
@@ -41,59 +30,18 @@ public class ClienteController implements Initializable {
 	Socket socket=null;
 	ArrayList<String> arquivosDoServidor =new ArrayList<String>();
 	
+	StubCliente cliente;
 	
-	Thread escuta;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
+		cliente = new StubCliente();
 		try {
-			socket = new Socket("127.0.0.1", 12345);
-		} catch (IOException e) {
+			arquivosDoServidor = cliente.receberLista();
+		} catch (RemoteException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		InetAddress inet = socket.getInetAddress();
-		System.out.println("HostAddress = "+inet.getHostAddress());
-		System.out.println("HostName = "+inet.getHostName());
-		
-		//Escutar servidor
-				  escuta=new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						try {
-							Object rcv;
-							Socket s=socket;
-							
-							while(true){
-								//Receber objeto do fluxo do socket
-								ObjectInputStream is=new ObjectInputStream(s.getInputStream());
-								rcv = is.readObject();
-							
-								if(rcv instanceof Arquivo) {
-									Arquivo arquivo= (Arquivo)  rcv;									
-									conteudoArquivo.setText(arquivo.getConteudo()); 
-								}
-								
-								if(rcv instanceof ArrayList) {
-									arquivosDoServidor= (ArrayList<String>) rcv;
-									listaDeArquivos.getItems().clear();
-									listaDeArquivos.getItems().addAll(arquivosDoServidor);
-								}
-								is=null;
-								
-							}
-						} catch (IOException e) {
-							e.printStackTrace();
-						} catch (ClassNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				});
-				
-			escuta.start();
-		
 		
 		 listaDeArquivos.getSelectionModel().selectedItemProperty().addListener(
 		                (observable, oldValue, newValue) -> {
---
+++ src/controller/InterfaceDoServidor.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+//Criando a interface (Skeleton) do servidor
+public interface InterfaceDoServidor extends Remote{
+
+	public ArrayList<String> receberLista() throws RemoteException;	
+}
